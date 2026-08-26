3723e31ed2f63b22876d8bab9218aefb4de64567
ramgariraghu123 <ramgariraghuveer@klh.edu.in>
2026-08-26 10:29:16 +0530

    Add files via upload

---
+++ Skill/shell.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char input[100];
+
+    while (1) {
+        printf("raghuveer@LAPTOP-SHKTDDI3:~$ ");
+
+        fgets(input, sizeof(input), stdin);
+
+        input[strcspn(input, "\n")] = '\0';
+
+        if (strcmp(input, "exit") == 0) {
+            printf("Exiting shell...\n");
+            break;
+        }
+
+        printf("You entered: %s\n", input);
+    }
+
+    return 0;
+}
